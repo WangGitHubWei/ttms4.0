48280efdb61ae283caaf77436b069e33fce591b3
wangwei <emailweiwang@163.com>
2017-10-07 14:50:36 +0800

    wangwei

---
+++ ttms4.0/src/main/java/cn/tedu/ttms/login/service/LoginService.java
@@ -0,0 +1,25 @@
+package cn.tedu.ttms.login.service;
+
+
+import cn.tedu.ttms.system.entity.User;
+
+public interface LoginService {
+
+	/**
+	 * 根据用户名判断用户是否存在
+	 * @param username
+	 * @return 
+	 */
+	User isExist(String username);
+
+	
+
+	/**
+	 * 校验密码
+	 * @param username
+	 * @param userpwd
+	 * @return 
+	 */
+//	User confirmPwd(String username, String userpwd);
+
+}
